bdd0f60a89f0c0d5034c9b03c320dc04e35d0c42
chengzh1 <cheng.zhang@sv.cmu.edu>
2014-03-11 15:41:46 -0400

    add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MailPost::Application.config.secret_key_base = 'b5862dfcc87b7923ffcd94e2bf02a82959c32dc5db1d40f6c9b50be71d024aacbb0575f5abf5cf5aa3200c967f3dd453f6521e3132b74602ec77f134b2db4402'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MailPost::Application.config.session_store :cookie_store, key: '_mail_post_session'
